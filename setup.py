1885a731adc6839b3dcabcc6138313724d2007dd
sexfrance <bwuuuuu@gmail.com>
2024-10-23 21:50:32 +0200

    Fixed setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description="A custom logger package",   
     long_description=open("README.md").read(),   
     long_description_content_type="text/markdown",
-    url="https://github.com/sexfrance/logmagix",  
+    url="https://github.com/sexfrance/LogMagix",  
     classifiers=[                          
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
