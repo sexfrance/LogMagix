d423b3afbbd74271247f90cade2854c797df990a
sexfrance <bwuuuuu@gmail.com>
2025-02-19 23:35:08 +0100

    Add requirements installation step in publish workflow

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 from logmagix.version import __version__
 
 setup(
-    name="logmagix",                   
-    version=__version__,                           
-    packages=find_packages(),               
-    install_requires=["colorama", "pystyle"],           
-    author="Sexfrance",                     
-    author_email="bwuuuuu@gmail.com",   
-    description="A custom logger package",   
+    name="logmagix",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=["colorama", "pystyle"],
+    author="Sexfrance",
+    author_email="bwuuuuu@gmail.com",
+    description="A custom logger package",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/sexfrance/LogMagix",  
-    classifiers=[                          
+    url="https://github.com/sexfrance/LogMagix",
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
